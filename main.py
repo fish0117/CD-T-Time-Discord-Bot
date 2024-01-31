d0f9e2a744dc799d42729f6f568d50ba583c14de
fish0117 <95156621+fish0117@users.noreply.github.com>
2024-01-31 13:42:51 +0000

    bot 基本架構

--- main.py
+++ main.py
@@ -3,22 +3,16 @@ import discord  #導入discord.py模組
 from discord.ext import commands  #導入指令模組
 bot_token = os.environ['Discord_Bot_Token']  #定義bot token
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all() #要求機器人權限
+bot = commands.Bot(command_prefix = "/", intents = intents) #定義機器人實體與指令前綴
 
-client = discord.Client(intents=intents) #定義機器人實體
-
-@client.event #機器人上線訊息
+@bot.event #機器人上線訊息
 async def on_ready():
-    print(f'登入身分為---> {client.user}')
+    print(f'登入身分為---> {bot.user}')
 
-@client.event
-async def on_message(message): #發送hello訊息
+@bot.command()
+async def hello(ctx): #發送hello訊息
   
-    if message.author == client.user:
-        return #防止機器人陷入迴圈
+    await ctx.send('你好~我是可愛的小蘑菇:)')
 
-    if message.content.startswith('/hello'):
-        await message.channel.send('你好~我是可愛的小蘑菇:)')
-
-client.run(bot_token)
+bot.run(bot_token)
